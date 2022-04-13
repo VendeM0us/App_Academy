bc5ca7224507847f06f5779039ccaeb09dd9342e
VendeMous <mayolito.waje08@gmail.com>
2022-04-13 13:32:33 +0800

    Add lose?, win?, [], []=, fringe, flag, and valid_pos? method

--- Ruby/minesweeper/interactive_board.rb
+++ Ruby/minesweeper/interactive_board.rb
@@ -1,4 +1,5 @@
 require_relative "board"
+require "byebug"
 
 class InteractiveBoard
     attr_reader :board, :board_hidden
@@ -6,7 +7,7 @@ class InteractiveBoard
     def initialize
         @board = Board.new
         @board.seed_bombs
-        @board_hidden = Array.new(9) { Array.new(9, " ") }
+        @board_hidden = Array.new(9) { Array.new(9, "*") }
     end
 
     def render
@@ -14,5 +15,50 @@ class InteractiveBoard
         board_hidden.each.with_index do |rows, i|
             puts "#{i} #{rows.join(" ")}"
         end
+    end
+
+    def [](pos)
+        x, y = pos
+        board_hidden[x][y]
+    end
+
+    def []=(pos, val)
+        x, y = pos
+        board_hidden[x][y] = val
+    end
+
+    def lose?(pos)
+        # :B means the square has bomb
+        board[pos] == :B
+    end
+
+    def win?
+        # if hidden squares are exactly 10, that means it hasn't touched hidden 10 bombs, so player wins
+        hidden = 0
+
+        board_hidden.each do |rows|
+            rows.each { |square| hidden += 1 if square == "*" }
+        end
+
+        hidden == 10
+    end
+
+    def fringe(pos)
+        #debugger
+        fringe_count = board.count_fringes(pos)
+        unless fringe_count == 0
+            self[pos] = fringe_count.to_s 
+        end
+    end
+
+    def flag(pos)
+        self[pos] = :F
+    end
+
+    def valid_pos?(pos)
+        # pass position validity in string state first before converting them to integers
+        idx_1, idx_2 = pos
+        valid_idx = ('0'..'8').to_a
+        valid_idx.include?(idx_1) && valid_idx.include?(idx_2)
     end
 end

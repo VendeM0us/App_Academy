564dc7a4a52c362abd27243e063b7462532d645e
VendeMous <mayolito.waje08@gmail.com>
2022-04-13 13:55:02 +0800

    Move valid_pos? to minesweeper.rb

--- Ruby/minesweeper/minesweeper.rb
+++ Ruby/minesweeper/minesweeper.rb
@@ -7,7 +7,19 @@ class MineSweeper
         @board = InteractiveBoard.new
     end
 
+    def valid_pos?(pos)
+        # pass position validity in string state first before converting them to integers
+        idx_1, idx_2 = pos
+        valid_idx = ('0'..'8').to_a
+        valid_idx.include?(idx_1) && valid_idx.include?(idx_2)
+    end
+
     def take_turn
         board.render
+
+        command = nil
+        pos = nil
+
+        until board.valid_pos?(pos) && 
     end
 end

098b94aecf1aaa976015714cc4102b3c7601a0d5
VendeMous <mayolito.waje08@gmail.com>
2022-04-13 13:02:16 +0800

    Add board rendering

--- Ruby/minesweeper/interactive_board.rb
+++ Ruby/minesweeper/interactive_board.rb
@@ -2,10 +2,17 @@ require_relative "board"
 
 class InteractiveBoard
     attr_reader :board, :board_hidden
-    
+
     def initialize
         @board = Board.new
         @board.seed_bombs
-        @board_hidden = Array.new(9) { Array.new(9) }
+        @board_hidden = Array.new(9) { Array.new(9, " ") }
+    end
+
+    def render
+        puts "  #{(0..8).to_a.join(" ")}"
+        board_hidden.each.with_index do |rows, i|
+            puts "#{i} #{rows.join(" ")}"
+        end
     end
 end

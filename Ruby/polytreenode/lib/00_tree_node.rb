b8d234d4755d2dc964a3caa68918ce2cf414880c
VendeMous <mayolito.waje08@gmail.com>
2022-04-20 15:52:16 +0800

    Add add_child method

--- Ruby/polytreenode/lib/00_tree_node.rb
+++ Ruby/polytreenode/lib/00_tree_node.rb
@@ -16,9 +16,20 @@ class PolyTreeNode
     end
 
     @parent = node
-    
+
     unless node.nil?
       node.children << self unless node.children.include?(self)
     end
+  end
+
+  def add_child(node)
+    if !self.children.include?(node)
+      self.children << node  
+      node.parent = self
+    end
+  end
+
+  def inspect
+    @value.inspect
   end
 end

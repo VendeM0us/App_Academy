d14bcc10f0d114abb5b4c1513a0a27f2f593246d
VendeMous <mayolito.waje08@gmail.com>
2022-04-20 15:03:43 +0800

    Make attr_reader public

--- Ruby/polytreenode/lib/00_tree_node.rb
+++ Ruby/polytreenode/lib/00_tree_node.rb
@@ -1,11 +1,9 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+
   def initialize(value)
     @parent = nil
     @children = []
     @value = value
   end
-
-  private
-
-  attr_reader :parent, :children, :value
 end

142d288b7775d71c2c23fa623d61034d8281c55d
VendeMous <mayolito.waje08@gmail.com>
2022-04-20 15:37:28 +0800

    Add parent= method

--- Ruby/polytreenode/lib/00_tree_node.rb
+++ Ruby/polytreenode/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class PolyTreeNode
   attr_reader :parent, :children, :value
 
@@ -5,5 +7,18 @@ class PolyTreeNode
     @parent = nil
     @children = []
     @value = value
+  end
+
+  def parent=(node)
+    unless self.parent.nil?
+      old_parent = self.parent
+      old_parent.children.delete(self)
+    end
+
+    @parent = node
+    
+    unless node.nil?
+      node.children << self unless node.children.include?(self)
+    end
   end
 end
